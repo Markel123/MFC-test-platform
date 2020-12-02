7e5256805561fe3d9f9e35d73a7829004eea9076
tink <1243941867@qq.com>
2020-12-02 20:30:08 +0800

    Create Resource.h

---
+++ Resource.h
@@ -0,0 +1,35 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 JINORun.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_JINORUN_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON_Open_Pic             1000
+#define IDC_BUTTON_Analyze_Pic          1001
+#define IDC_BUTTON_Last_Pic             1004
+#define IDC_BUTTON_Next_Pic             1005
+#define IDC_COMBO_Cam_Num               1007
+#define IDC_COMBO_Methode_Type          1008
+#define IDC_BUTTON_ROI_righten          1009
+#define IDC_BUTTON_ROI_Leften           1010
+#define IDC_BUTTON_ROI_Down             1011
+#define IDC_BUTTON_Up                   1012
+#define IDC_SLIDER_ROI_Step             1013
+#define IDC_TREE_Choose_Item            1017
+#define IDC_STATIC_Show_Pic             1020
+#define IDC_BUTTON_Reset_ROI            1021
+#define IDC_STATIC_Show_Hist            1025
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
